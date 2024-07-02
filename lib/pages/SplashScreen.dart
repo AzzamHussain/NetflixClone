2a006be4878f4649ac9100bf2294492dbb23ef94
AzzamHussain <azzamhussain70@gmail.com>
2024-07-02 16:53:24 +0500

    navbar done

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:netflixclone/widgets/Bottom_Nav_Bar.dart';
 
-class Splashscreen extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BottomNavBar(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,3 +30,13 @@ class Splashscreen extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+
+// class Splashscreen extends StatefulWidget {
+//   const Splashscreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+ 
+//   }
+// }
---
+++ lib/widgets/Bottom_Nav_Bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          bottomNavigationBar: Container(
+            color: Colors.black,
+            height: 70,
+            child: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home),
+                  text: "Home",
+                ),
+                Tab(
+                  icon: Icon(Icons.search),
+                  text: "Search",
+                ),
+                Tab(
+                  icon: Icon(Icons.photo_library_outlined),
+                  text: "New & Hot",
+                ),
+              ],
+              indicatorColor: Colors.transparent,
+            ),
+          ),
+        ));
+  }
+}
